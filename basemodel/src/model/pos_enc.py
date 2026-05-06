38889db35e8baeaf8346c633bfc881d8b8f4f04d
Adin Farel <fareladin00@gmail.com>
2026-05-06 23:20:28 +0700

    feat(model): add RoPE architecture modern LLM now

--- basemodel/src/model/pos_enc.py
+++ basemodel/src/model/pos_enc.py
@@ -14,5 +14,25 @@ class LearnedPositionalEnc(nn.Module):
         self.pos_emb = nn.Embedding(block_size, embedding_dim)
     
     def forward(self, x: torch.tensor):
-        arange = torch.arange(self.block_size, device=x.device)
+        B, T, C = x.shape
+        arange = torch.arange(T, device=x.device)
         return self.pos_emb(arange) + x
+
+class RotaryPositionalEncoding(nn.Module):
+    def __init__(self, embedding_dim: int, base=10000.0):
+        super().__init__()
+        self.dim = embedding_dim
+        inv_freq = 1.0 / (base ** (torch.arange(0, self.dim, 2, dtype=torch.float32) / self.dim))
+        self.register_buffer("inv_freq", inv_freq)
+    
+    def forward(self, x: torch.tensor):
+        B, T, C = x.shape
+        assert C == self.dim
+        l = torch.arange(T, dtype=self.theta.dtype) # [0, 1, 2, ..., L]
+        theta = torch.einsum("i,j->ij", l, self.theta)
+        hat_theta = torch.cat([theta, theta], axis=-1)
+        sin = torch.sin(hat_theta)
+        cos = torch.cos(hat_theta)
+        xu, xd = x[..., : C // 2], x[..., C // 2 :]
+        hatx = torch.cat([-xd, xu], axis=-1)
+        return x * cos + hatx * sin
