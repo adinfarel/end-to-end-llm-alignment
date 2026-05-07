7509b098c89c8a05f6024fb326124d88d10cfea5
Adin Farel <fareladin00@gmail.com>
2026-05-07 21:39:23 +0700

    fix(model): fix some bug and optimalize

--- basemodel/src/model/pos_enc.py
+++ basemodel/src/model/pos_enc.py
@@ -22,17 +22,17 @@ class RotaryPositionalEncoding(nn.Module):
     def __init__(self, embedding_dim: int, base=10000.0):
         super().__init__()
         self.dim = embedding_dim
-        inv_freq = 1.0 / (base ** (torch.arange(0, self.dim, 2, dtype=torch.float32) / self.dim))
+        inv_freq = 1.0 / (base ** (torch.arange(0, self.dim, 2, dtype=torch.float32) / self.dim)) # (embedding_dim / 2)
         self.register_buffer("inv_freq", inv_freq)
     
     def forward(self, x: torch.tensor):
         B, T, C = x.shape
         assert C == self.dim
         l = torch.arange(T, dtype=self.theta.dtype) # [0, 1, 2, ..., L]
-        theta = torch.einsum("i,j->ij", l, self.theta)
-        hat_theta = torch.cat([theta, theta], axis=-1)
-        sin = torch.sin(hat_theta)
-        cos = torch.cos(hat_theta)
+        theta = torch.einsum("i,j->ij", l, self.inv_freq) # (l, inv_freq)
+        hat_theta = torch.cat([theta, theta], axis=-1) # (l, inv_freq + inv_freq)
+        sin = torch.sin(hat_theta) # (l, hat_theta)
+        cos = torch.cos(hat_theta) # (l, hat_theta)
         xu, xd = x[..., : C // 2], x[..., C // 2 :]
         hatx = torch.cat([-xd, xu], axis=-1)
         return x * cos + hatx * sin
