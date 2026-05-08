633d4333e7d9f6beaa86f155367a6e5136dab8ca
Adin Farel <fareladin00@gmail.com>
2026-05-08 23:15:19 +0700

    update(model): inject new architecture such that RMSNorm, GQA, RoPE, and SwiGLU

--- basemodel/src/model/block.py
+++ basemodel/src/model/block.py
@@ -7,18 +7,18 @@ A single transforme block that contains all layer of transformers
 import torch
 import torch.nn as nn
 from basemodel.src.model import (
-    multiheadattn, ffwd
+    multiheadattn, ffwd, normalization
 )
 
 class Block(nn.Module):
-    def __init__(self, n_embd, n_heads, dropout, block_size):
+    def __init__(self, n_embd, n_heads, dropout):
         super().__init__()
         assert n_embd % n_heads == 0, "n_heads must be divide n_embd with result = 0"
-        head_size = n_embd // n_heads
-        self.attn = multiheadattn.MultiHeadAttention(num_heads=n_heads, head_size=head_size, n_embd=n_embd, dropout=dropout, block_size=block_size)
+        # head_size = n_embd // n_heads
+        self.attn = multiheadattn.GroupQueryAttention(n_embd=n_embd, n_q_head=n_heads, n_kv_heads=n_heads//2, dropout=dropout)
         self.ffwd = ffwd.FeedForward(n_embd=n_embd, dropout=dropout)
-        self.ln1 = nn.LayerNorm(n_embd)
-        self.ln2 = nn.LayerNorm(n_embd)
+        self.ln1 = normalization.RMSNorm(n_embd)
+        self.ln2 = normalization.RMSNorm(n_embd)
     
     def forward(self, x):
         x = x + self.attn(self.ln1(x))
