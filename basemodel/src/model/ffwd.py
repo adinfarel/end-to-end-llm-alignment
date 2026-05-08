4f85fed5374c7df1375438347e337dfaf26147d1
Adin Farel <fareladin00@gmail.com>
2026-05-08 22:41:28 +0700

    update(model): add SwiGLUFFN for mdern architecture in Non-linearity function

--- basemodel/src/model/ffwd.py
+++ basemodel/src/model/ffwd.py
@@ -6,6 +6,7 @@ To get non-linear output at 4 times space from n_embed and squash (projection) a
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 class FeedForward(nn.Module):
     def __init__(self, n_embd, dropout):
@@ -19,3 +20,19 @@ class FeedForward(nn.Module):
     
     def forward(self, x):
         return self.ffwd(x)
+
+class SwiGLUFFN(nn.Module):
+    def __init__(self, n_embd, dropout):
+        super().__init__()
+        self.n_embd = int(n_embd * (8/3))
+        self.gate = nn.Linear(n_embd, self.n_embd, bias=False)
+        self.value = nn.Linear(n_embd, self.n_embd, bias=False)
+        self.down_proj = nn.Linear(self.n_embd, n_embd, bias=False)
+        self.dropout = nn.Dropout(dropout)
+    
+    def forward(self, x):
+        value = self.value(x)
+        gate = F.silu(self.gate(x)) # Formula: x * sigmoid(x) = x * (1 / (1 + exp(-x))) = x / (1 + exp(-x))
+        hidden = value * gate
+        out = self.down_proj(hidden)
+        return self.dropout(out)
