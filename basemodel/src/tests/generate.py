c9a673b2784d4e5e8a1eeb8d9db2302f674cb7cc
Adin Farel <fareladin00@gmail.com>
2026-05-10 23:23:54 +0700

    feat(tests): testing model for generate input

---
+++ basemodel/src/tests/generate.py
@@ -0,0 +1,54 @@
+'''
+src/tests/generate.py
+
+Generate next-token prediction for testing the model
+'''
+
+import torch
+import os
+from basemodel.src.model.gpt import AlmondGPTModel
+from basemodel.src.tokenizer.bpe import AlmondTokenizerGPT
+from utils.common import load_model, load_yaml
+
+# -------------------------------
+YAML_PATH = 'basemodel/config.yaml'
+CONFIG = load_yaml(YAML_PATH)
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+# -------------------------------
+
+def load_model_and_tokenizer():
+    model = AlmondGPTModel(config_path=YAML_PATH).to(device=DEVICE)
+    model = load_model(model=model, file_path=os.path.join(CONFIG['models']['training']['model_save_dir'], 'best_model.pt'), device=DEVICE)
+    model.eval()
+    
+    tokenizer = AlmondTokenizerGPT(config_path=YAML_PATH)
+    tokenizer.load(
+        vocab_path=CONFIG['tokenizer']['vocab_path'] + 'vocab.json',
+        merges_path=CONFIG['tokenizer']['merges_path'] + 'merges.json',
+    )
+    
+    return model, tokenizer
+    
+def generate_test(model: AlmondGPTModel, tokenizer: AlmondTokenizerGPT, prompt: str, max_new_tokens: int = 1000):
+    '''Inference model for next-token prediction'''
+    
+    ids = tokenizer.encode(prompt)
+    idx = torch.tensor([ids], dtype=torch.long).to(DEVICE)
+    
+    with torch.no_grad():
+        output = model.generate(idx, max_new_tokens=max_new_tokens)
+    
+    return tokenizer.decode(output[0].tolist())
+
+if __name__ == "__main__":
+    print("Loading model and tokenizer...")
+    model, tokenizer = load_model_and_tokenizer()
+    
+    while True:
+        prompt = input("Enter a prompt (or 'exit' to quit): ")
+        if prompt.lower() == 'exit':
+            print("Exiting...")
+            break
+        
+        result = generate_test(model, tokenizer, prompt)
+        print(f"Generated text:\n{result}\n")
