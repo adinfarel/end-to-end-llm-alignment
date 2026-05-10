4a77c3fad0c52cf6381572879ec8031f16143844
Adin Farel <fareladin00@gmail.com>
2026-05-10 20:39:43 +0700

    feat(pipeline): whole orchestration of this project, just use this file and yall get how main idea of this project

---
+++ basemodel/src/pipeline/main.py
@@ -0,0 +1,36 @@
+'''
+src/pipeline/main.py
+
+This is the main file for the entire pipeline of training the tokenizer and the model. It will call the train.py files in both tokenizer and model directories to execute the training process.
+'''
+
+from basemodel.src.tokenizer.train import train_tokenizer
+from basemodel.src.data.load import load_datasets
+from basemodel.src.data.processed import processed_corpus
+from basemodel.src.model.train import main as train_model
+from basemodel.src.model.train import TrainConfig
+from utils.common import load_yaml
+
+# -------------------------------
+YAML_PATH = "basemodel/config.yaml"
+CONFIG = load_yaml(YAML_PATH)
+# -------------------------------
+
+if __name__ == "__main__":
+    # Load dataset from HuggingFace and save as .txt file
+    load_datasets(yaml_path=YAML_PATH)
+    
+    # Train tokenizer to get vocab and merges
+    raw_data_path = CONFIG['dataset']['raw_save_path'] + 'tinystories.txt'
+    vocab_path = CONFIG['tokenizer']['vocab_path'] + 'vocab.json'
+    merges_path = CONFIG['tokenizer']['merges_path'] + 'merges.json'
+    train_tokenizer(raw_data_path, vocab_path, merges_path)
+    CONFIG = load_yaml(YAML_PATH)  # Reload config to get updated vocab size
+    
+    # Process raw text data to token IDs and save as binary file
+    processed_data_path = CONFIG['dataset']['processed_save_path'] + 'corpus.bin'
+    processed_corpus(raw_data_path, processed_data_path, vocab_path, merges_path)
+    
+    # The main idea of this project >.<
+    train_config = TrainConfig.config(yaml_path=YAML_PATH)
+    train_model(config=train_config, early_stopping=train_config.early_stopping)
