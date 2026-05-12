ccce55423de73092767284a05f6bc130bac7f617
Adin Farel <fareladin00@gmail.com>
2026-05-12 22:44:00 +0700

    feat(data): add InstructionDataset and collate fn for plugin into DataLoader

--- sftmodel/src/data/datasets.py
+++ sftmodel/src/data/datasets.py
@@ -11,7 +11,7 @@ from torch.utils.data import Dataset
 
 class InstructionDataset(Dataset):
     def __init__(self, data_list: list, tokenizer: AlmondTokenizerGPT, device: torch.device) -> None:
-        self.data = data_list
+        self.data = data_list # List of raw text data samples
         self.encoded_texts = []
         
         for text in self.data:
